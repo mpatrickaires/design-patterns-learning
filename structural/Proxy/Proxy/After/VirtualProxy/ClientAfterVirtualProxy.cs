80c52e0c485d041c659c74b362d67e329c671ffb
Matheus Patrick <mpatrickaires@gmail.com>
2022-08-29 20:41:04 -0300

    Added explanation for Virtual Proxy example

--- structural/Proxy/Proxy/After/VirtualProxy/ClientAfterVirtualProxy.cs
+++ structural/Proxy/Proxy/After/VirtualProxy/ClientAfterVirtualProxy.cs
@@ -1,5 +1,18 @@
 ﻿using Proxy.After.VirtualProxy.Proxy;
 
+/*
+ * Now, let's take a look at this example: a new class was created, the DbMonitorProxy, which implements
+ * the same interface as the original DbMonitor. This proxy contains a wrapped object of DbMonitor,
+ * to whom all the methods call will be delegated.
+ * The big point is, our proxy will only instantiate its wrapped object when a method is indeed called,
+ * but never before. This allows us to create this object without having to wait the overhead it does
+ * of connecting to a database, only doing so if our user indeed goes to a routine where this is 
+ * necessary.
+ * This way, we implemented what is called a 'lazy loading', that is, a class is only instantiated when
+ * some method of it is really called. This is a great way to avoid demanding tasks that happens at the 
+ * constructor.
+ */
+
 namespace Proxy.After.VirtualProxy
 {
     public static class ClientAfterVirtualProxy
--- structural/Proxy/Proxy/Before/VirtualProxy/ClientBeforeVirtualProxy.cs
+++ structural/Proxy/Proxy/Before/VirtualProxy/ClientBeforeVirtualProxy.cs
@@ -1,5 +1,21 @@
 ﻿using Proxy.Common.Services;
 
+/*
+ * Imagine we have a class that connects to a database to do some monitoring job, like checking the
+ * status and open connections. 
+ * We will always have an object of this class available to do its job, however a problem is that
+ * the class connects to the database as soon as it is instantiated, that is, this connection
+ * happens at the constructor.
+ * In our scenario, establishing a connection to a database is a very 
+ * demanding task, that takes some time to complete, basically freezing the application.
+ * This much likely causes an unecessary overhead, as the user would have to always wait the
+ * connection completion everytime the application is started, this even if no operation envolving
+ * this monitor is done.
+ * 
+ * But... imagine if we could do a way to delay the call to this class constructor to only when
+ * we indeed call a method: this is what we'll see at the 'After' example.
+ */
+
 namespace Proxy.Before.VirtualProxy
 {
     public static class ClientBeforeVirtualProxy
