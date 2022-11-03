1fd90da1f4e8f2daba0ce58d0eb402b63449ae73
Matheus Patrick <mpatrickaires@gmail.com>
2022-11-03 19:40:51 -0300

    Finished Strategy pattern example

--- behavioral/Strategy/Strategy/After/ClientAfter.cs
+++ behavioral/Strategy/Strategy/After/ClientAfter.cs
@@ -1,6 +1,23 @@
 ﻿using Strategy.After.Services;
 using Strategy.After.Strategies;
 
+/*
+ * Now, this is the usage of the Strategy pattern, so let's understand it.
+ * Firstly, we extracted the different behaviours of each condition statement into their own classes.
+ * For example, the routine of fabricating a car, a motorcycle and a bicycle is now extracted into a
+ * class, which represent the strategy. Now, for the VehicleFactory class (which in the strategy pattern
+ * would be refered as the context class), it doens't need to have the multiple conditions statements
+ * for the type of the vehicle anymore, the only things it needs is to reference the strategy for the 
+ * necessary behavior. The context class (VehicleFactory) will communicate with the strategy only 
+ * through the interface, which allows the strategy to be defined at runtime.
+ * In all that, the client will then define which strategy it wants the context class to execute (the
+ * client needs to have knowledge of the objective of each strategy), which can be done by Dependency 
+ * Injection.
+ * So that's it, by doing this implementation, when the fabrication of a new vehicle becomes necessary,
+ * we will not need to modify the VehicleFactory class, just by creating a new strategy class and 
+ * including it for the client to inject, we will already easily include this new behavior.
+ */
+
 namespace Strategy.After
 {
     public static class ClientAfter
--- behavioral/Strategy/Strategy/Before/ClientBefore.cs
+++ behavioral/Strategy/Strategy/Before/ClientBefore.cs
@@ -5,7 +5,18 @@ using Strategy.Before.Services;
  * Let's say we have an application that is able to fabricate vehicles (wow, pretty advanced, uh?) through a factory service. Its capabilities
  * is to fabricate cars, motorcycles and bicycles, and it takes the decision to which one is going to be fabricated by an enum that defines the
  * type of the vehicle.
- * Well, each vehicle has its particularities in the fabrication process.
+ * Well, each vehicle has its particularities in the fabrication process, for example:
+ * - A car needs a motor, four wheels and fuel.
+ * - A motorcycle needs a motor, two wheels and fuel.
+ * - A bicycle only needs two wheels.
+ * This is where the problem resides, because each fabrication takes different paths and we would need
+ * to throw different conditions for that in the code; this would quickly create a boilerplate code,
+ * where if we wanted to add the fabrication of a new vehicle, we would need to change the existing 
+ * class and with it have the risk of breaking the code for the other fabrications.
+ * 
+ * We can see that it would quickly become a problem and create a big difficulty for extending the code,
+ * while breaking the Open Closed Principle.
+ * With that in mind, let's take a look at how we can use the Strategy pattern to solve this situation.
  */
 
 namespace Strategy.Before
